2dbd8b9298cd7513a13ee7548bb2a71f1f9fb023
harissmajlovic <smajlovicharis@hotmail.com>
2021-01-07 14:24:09 -0800

    Problem statement updated

--- algorithms/interface.py
+++ algorithms/interface.py
@@ -6,20 +6,21 @@ from algorithms.intersections import find_intersections_naive, find_intersection
 def rotors_finder(
         rotors: list,
         intersections_finder: object,
-        verbose: bool = False) -> list:
+        verbose: bool = True) -> list:
     intersections: dict = intersections_finder(rotors)
     busy_periods: list = find_busy_periods(intersections)
     merged_busy_periods: list = merge_segments(busy_periods)
 
     if verbose:
-        from draw import draw_circles, draw_intersections, turtle, math
+        import math
+        # from draw import draw_circles, draw_intersections, turtle, math
 
-        draw_circles(rotors)
-        draw_intersections(intersections)
+        # draw_circles(rotors)
+        # draw_intersections(intersections)
 
         print(f'Busy periods: {[[e * 180 / math.pi for e in t] for t in busy_periods]}')
         print(f'Busy periods merged: {[[e * 180 / math.pi for e in t] for t in merged_busy_periods]}')
-        turtle.done()
+        # turtle.done()
 
     return merged_busy_periods
 
